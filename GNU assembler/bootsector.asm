1b0eac11a71b5eb973cbc3475dc667dfcb676139
mariogollub <gollub.mario@gmx.de>
2024-05-27 21:35:07 +0200

    Create bootsector.asm

---
+++ GNU assembler/bootsector.asm
@@ -0,0 +1,23 @@
+.code16
+.global init
+
+init:
+    mov $msg, %si
+	mov $0xe, %ah
+
+print_char:
+	lodsb
+	cmp $0, %al
+	je done
+	int $ox10
+	jmp print_char
+
+done:
+    hlt
+
+msg:
+	.asciz "Hallo Welt"
+	
+.fill 510-(.-init), 1, 0
+
+.word 0xaa55
